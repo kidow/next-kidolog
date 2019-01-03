8eb94880e9646ef3b3b6a35b4f94e609aa83f81c
kidow <wcgo2ling@hanmail.net>
2019-01-03 17:29:56 +0900

    server middlewares basic set

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
+require('dotenv').config()
 const express = require('express')
 const next = require('next')
+const morgan = require('morgan')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const { PORT, NODE_ENV, COOKIE_KEY } = process.env
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
+const port = parseInt(PORT, 10) || 3000
+const dev = NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
+
+  if (NODE_ENV === 'development') {
+    server.use(morgan('dev'))
+  }
+  server.use(express.json())
+  server.use(express.urlencoded({ extended: false }))
+  server.use(cookieParser(COOKIE_KEY))
+  server.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: COOKIE_KEY,
+      cookie: {
+        maxAge: 8640000,
+        secure: false
+      }
+    })
+  )
 
   server.get('*', (req, res) => {
     return handle(req, res)
