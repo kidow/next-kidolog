d3aefc3285bad2372ca38762a4521faefb2718c5
kidow <wcgo2ling@hanmail.net>
2019-01-04 02:24:06 +0900

    Footer component set

---
+++ components/organisms/Footer/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Copyright (c) Kidolog.com, All rights reserved.
+    </div>
+  )
+}
+
+export default Footer
