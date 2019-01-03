1e214275a84c26cd690852842ba03bbc76a1800b
kidow <wcgo2ling@hanmail.net>
2019-01-04 02:43:08 +0900

    PostTemplate component set

---
+++ components/templates/PostTemplate/index.js
@@ -0,0 +1,7 @@
+import './index.scss'
+
+const PostTemplate = ({ children }) => {
+  return <div className="post-template">{children}</div>
+}
+
+export default PostTemplate
--- components/templates/index.js
+++ components/templates/index.js
@@ -1,3 +1,4 @@
 export { default as EditorTemplate } from './EditorTemplate'
 export { default as ListTemplate } from './ListTemplate'
 export { default as PageTemplate } from './PageTemplate'
+export { default as PostTemplate } from './PostTemplate'
