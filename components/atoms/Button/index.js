5553aefb55b171690b73059603544bcd9501d442
kidow <wcgo2ling@hanmail.net>
2019-01-04 00:38:24 +0900

    button file name change

--- components/atoms/Button/index.js
+++ components/atoms/Button/index.js
@@ -1,1 +1,14 @@
-export { default } from './Button';
+import styles from './index.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
+const Button = ({ children, theme, ...rest }) => {
+  return (
+    <div className={cx('button__container', theme)} {...rest}>
+      {children}
+    </div>
+  )
+}
+
+export default Button
