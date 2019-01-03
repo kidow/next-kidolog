6a20730c068a3779697494154c8677bc5969ad8e
kidow <wcgo2ling@hanmail.net>
2019-01-03 17:10:53 +0900

    global style set

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+import '../lib/styles/index.scss'
+
+class CustomApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
+
+export default CustomApp
