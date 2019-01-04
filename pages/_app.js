72c23580d39484b0c183e438c07837d70b169d64
kidow <wcgo2ling@hanmail.net>
2019-01-05 02:27:33 +0900

    베포 환경에서는 redux devtools disabled

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,23 @@ import App, { Container } from 'next/app'
 import Head from 'next/head'
 import 'styles/index.scss'
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
 import reducer from 'store'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import penderMiddleware from 'redux-pender'
 
-const makeStore = () =>
-  createStore(reducer, composeWithDevTools(applyMiddleware(penderMiddleware())))
+const makeStore = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return createStore(
+      reducer,
+      composeWithDevTools(applyMiddleware(penderMiddleware()))
+    )
+  } else {
+    return createStore(reducer, compose(applyMiddleware(penderMiddleware())))
+  }
+}
 
 class CustomApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
