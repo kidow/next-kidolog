3691d5bc047ee458381dec72c84e1de63628c7f7
kidow <wcgo2ling@hanmail.net>
2019-01-04 12:13:15 +0900

    css loader 추가

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 const path = require('path')
 
-module.exports = withSass({
-  webpack(config) {
-    config.resolve.alias['styles'] = path.join(__dirname, 'lib/styles')
-    config.resolve.alias['components'] = path.join(__dirname, 'components')
-    config.resolve.alias['containers'] = path.join(__dirname, 'containers')
-    config.resolve.alias['store'] = path.join(__dirname, 'store')
-    config.resolve.alias['api'] = path.join(__dirname, 'lib/api')
-    config.resolve.alias['route'] = path.join(__dirname, 'route.js')
-    return config
-  }
-})
+module.exports = withCSS(
+  withSass({
+    webpack(config) {
+      config.resolve.alias['styles'] = path.join(__dirname, 'lib/styles')
+      config.resolve.alias['components'] = path.join(__dirname, 'components')
+      config.resolve.alias['containers'] = path.join(__dirname, 'containers')
+      config.resolve.alias['store'] = path.join(__dirname, 'store')
+      config.resolve.alias['api'] = path.join(__dirname, 'lib/api')
+      config.resolve.alias['route'] = path.join(__dirname, 'route.js')
+      return config
+    }
+  })
+)
