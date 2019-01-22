9a03e6e9076e7fc2fad0c956f645dae71c6fe939
kidow <wcgo2ling@hanmail.net>
2019-01-23 00:01:45 +0900

    일단인 타입스크립트 보류

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
-const withTypescript = require('@zeit/next-typescript')
 const path = require('path')
 
-module.exports = withTypescript(
-  withSass(
-    withCSS({
-      webpack(config) {
-        config.resolve.alias['components'] = path.join(__dirname, 'components')
-        config.resolve.alias['containers'] = path.join(__dirname, 'containers')
-        config.resolve.alias['lib'] = path.join(__dirname, 'lib')
-        config.resolve.alias['store'] = path.join(__dirname, 'lib/store')
-        config.resolve.alias['styles'] = path.join(__dirname, 'lib/styles')
-        config.resolve.alias['api'] = path.join(__dirname, 'lib/api')
-        return config
-      }
-    })
-  )
+module.exports = withSass(
+  withCSS({
+    webpack(config) {
+      config.resolve.alias['components'] = path.join(__dirname, 'components')
+      config.resolve.alias['containers'] = path.join(__dirname, 'containers')
+      config.resolve.alias['lib'] = path.join(__dirname, 'lib')
+      config.resolve.alias['store'] = path.join(__dirname, 'lib/store')
+      config.resolve.alias['styles'] = path.join(__dirname, 'lib/styles')
+      config.resolve.alias['api'] = path.join(__dirname, 'lib/api')
+      return config
+    }
+  })
 )
