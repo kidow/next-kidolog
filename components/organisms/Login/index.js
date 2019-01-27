17ee979c8d55ca6f276cfff9c7a1d492157b1122
kidow <wcgo2ling@hanmail.net>
2019-01-27 22:27:18 +0900

    로그인 회원가입 UI

--- components/organisms/Login/index.js
+++ components/organisms/Login/index.js
@@ -1,3 +1,40 @@
 import './index.scss'
+import { Input, Label, Button, IconButton } from 'components/atoms'
+import {
+  FaFacebook,
+  FaGoogle,
+  FaGithub,
+  FaKickstarter,
+  FaNeos
+} from 'react-icons/fa'
 
-export default () => <div className="login__container">Login</div>
+export default ({ error }) => (
+  <div className="login__container">
+    <div className="login__title">로그인</div>
+    <div>
+      <Label>이메일</Label>
+      <Input theme="auth" placeholder="이메일" />
+    </div>
+    <div>
+      <Label>비밀번호</Label>
+      <Input theme="auth" placeholder="비밀번호" type="password" />
+    </div>
+    {error && <div className="error__container">{error}</div>}
+    <Button theme="auth">로그인</Button>
+    <IconButton IconName={FaFacebook} size={25} theme="facebook">
+      페이스북
+    </IconButton>
+    <IconButton IconName={FaGoogle} size={25} theme="google">
+      구글
+    </IconButton>
+    <IconButton IconName={FaGithub} size={25} theme="github">
+      깃허브
+    </IconButton>
+    <IconButton IconName={FaKickstarter} size={25} theme="kakao">
+      카카오
+    </IconButton>
+    <IconButton IconName={FaNeos} size={25} theme="naver">
+      네이버
+    </IconButton>
+  </div>
+)
--- components/organisms/SignUp/index.js
+++ components/organisms/SignUp/index.js
@@ -1,8 +1,14 @@
 import './index.scss'
 import { Input, Label, Button, IconButton } from 'components/atoms'
-import { FaFacebook, FaTwitter, FaGithub } from 'react-icons/fa'
+import {
+  FaFacebook,
+  FaGoogle,
+  FaGithub,
+  FaKickstarter,
+  FaNeos
+} from 'react-icons/fa'
 
-export default () => (
+export default ({ error }) => (
   <div className="signup__container">
     <div className="signup__title">회원가입</div>
     <div>
@@ -11,17 +17,24 @@ export default () => (
     </div>
     <div>
       <Label>비밀번호</Label>
-      <Input theme="auth" placeholder="비밀번호" />
+      <Input theme="auth" placeholder="비밀번호" type="password" />
     </div>
-    <Button>회원가입</Button>
-    <IconButton IconName={FaFacebook} size={15} theme="facebook">
+    {error && <div className="error__container">{error}</div>}
+    <Button theme="auth">회원가입</Button>
+    <IconButton IconName={FaFacebook} size={25} theme="facebook">
       페이스북
     </IconButton>
-    <IconButton IconName={FaTwitter} size={15} theme="google">
+    <IconButton IconName={FaGoogle} size={25} theme="google">
       구글
     </IconButton>
-    <IconButton IconName={FaGithub} size={15} theme="github">
+    <IconButton IconName={FaGithub} size={25} theme="github">
       깃허브
+    </IconButton>
+    <IconButton IconName={FaKickstarter} size={25} theme="kakao">
+      카카오
+    </IconButton>
+    <IconButton IconName={FaNeos} size={25} theme="naver">
+      네이버
     </IconButton>
   </div>
 )
