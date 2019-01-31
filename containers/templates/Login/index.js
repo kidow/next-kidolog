7bd728562f38e84426626087b81ff09bbddd8bb4
kidow <wcgo2ling@hanmail.net>
2019-02-01 00:00:51 +0900

    redux 연결전

--- containers/templates/Login/index.js
+++ containers/templates/Login/index.js
@@ -1,5 +1,9 @@
 import { LoginTemplate } from 'components/templates'
 
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as authActions from 'store/auth'
+
 class LoginTemplateContainer extends React.Component {
   render() {
     return <LoginTemplate />
--- containers/templates/SignUp/index.js
+++ containers/templates/SignUp/index.js
@@ -1,5 +1,9 @@
 import { SignUpTemplate } from 'components/templates'
 
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as authActions from 'store/auth'
+
 class SignUpTemplateContainer extends React.Component {
   render() {
     return <SignUpTemplate />
