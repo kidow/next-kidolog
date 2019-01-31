d72174ce9928aaf1ec7044fcaab43720a65bd5a3
kidow <wcgo2ling@hanmail.net>
2019-01-31 09:05:32 +0900

    model user 재구성

--- models/index.js
+++ models/index.js
@@ -16,6 +16,6 @@ module.exports = () => {
   )
 
   require('./post')
-  require('./account')
+  require('./user')
   require('./comment')
 }
---
+++ models/user/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const User = new mongoose.Schema({
+  email: String,
+  password: String,
+  profile: {
+    nickname: String,
+    thumbnail: { type: String, default: '/static/default_thumbnail.png' }
+  },
+  social: {
+    facebook: { id: String, accessToken: String },
+    google: { id: String, accessToken: String },
+    github: { id: String, accessToken: String },
+    naver: { id: String, accessToken: String },
+    kakao: { id: String, accessToken: String }
+  },
+  createdAt: { type: Date, default: Date.now }
+})
+
+User.statics.findByEmail = require('./findByEmail')
+User.statics.findByNickname = require('./findByNickname')
+User.statics.findBySocialId = require('./findBySocialId')
+User.statics.localRegister = require('./localRegister')
+User.statics.socialRegister = require('./socialRegister')
+
+User.methods.validatePassword = require('./validatePassword')
+User.methods.encodeToken = require('./encodeToken')
+
+module.exports = mongoose.model('User', User)
